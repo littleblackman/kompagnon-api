59dba39e513237a99d4e67140b45b437f8ef4d37
littleBlackMan <sandyrazafitrimo@gmail.com>
2025-03-11 12:57:45 +0100

    add Redis

--- src/Controller/MetadataController.php
+++ src/Controller/MetadataController.php
@@ -10,18 +10,31 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use App\Service\ProjectService;
-
+use Symfony\Contracts\Cache\CacheInterface;
 class MetadataController extends AbstractController
 {
 
     #[Route('/api/metadata', name: 'get_metadata', methods: ['GET'])]
-    public function getMetadata(CriteriaRepository $criteriaRepository, PersonnageRepository $personnageRepository, StatusRepository $statusRepository): JsonResponse
+    public function getMetadata(
+        CriteriaRepository $criteriaRepository,
+        PersonnageRepository $personnageRepository,
+        StatusRepository $statusRepository,
+        CacheInterface $cache
+    ): JsonResponse
     {
+
+        $criterias = $cache->get('criterias', function() use ($criteriaRepository) {
+            return $criteriaRepository->findAll();
+        });
+        $status = $cache->get('status', function() use ($statusRepository, $criteriaRepository) {
+            return $statusRepository->findAll();
+        });
+
        return $this->json(
            [
-               'criterias' => $criteriaRepository->findAll(),
+               'criterias' => $criterias,
+               'status' =>$status,
                'personnages' => $personnageRepository->findAll(),
-               'status' => $statusRepository->findAll(),
            ],
            200,
            [],
